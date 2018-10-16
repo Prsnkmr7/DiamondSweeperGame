3fcdeba8abbe022c67e3b5420ee4fe4b44a4ecf6
DESKTOP-L26MMI9\Prasun <prasun@moonraft.com>
2018-10-17 04:53:22 +0530

    Make Hint Work

--- js/main.js
+++ js/main.js
@@ -8,20 +8,20 @@ quintBuild.config(function ($stateProvider, $urlRouterProvider) {
 
     $stateProvider
 
-    .state('Info', {
-        url: '/',
-        templateUrl: 'Templates/info.html'
-    })
-    .state('home', {
-        url: '/home',
-        templateUrl: 'Templates/home.html'
-    })
-        
+        .state('Info', {
+            url: '/',
+            templateUrl: 'Templates/info.html'
+        })
+        .state('home', {
+            url: '/home',
+            templateUrl: 'Templates/home.html'
+        })
+
 });
 
 // Info Controller Logic
 
-quintBuild.controller('infoController', ['$scope', '$state', '$rootScope', function ($scope, $state, $rootScope ) {
+quintBuild.controller('infoController', ['$scope', '$state', '$rootScope', function ($scope, $state, $rootScope) {
     $scope.user = {};
     $scope.submitForm = function (val) {
         if (val) {
@@ -35,9 +35,12 @@ quintBuild.controller('infoController', ['$scope', '$state', '$rootScope', funct
 
 // Home Controller logic
 
-quintBuild.controller('homeController', ['$scope','$rootScope', 'quintAPIservice', function ($scope,$rootScope, quint) {
+quintBuild.controller('homeController', ['$scope', '$rootScope', 'quintAPIservice', function ($scope, $rootScope, quint) {
     $scope.listItems = [];
     $scope.user = {};
+    var clickedData;
+
+    // Initial Functional called on load
     $scope.init = function () {
         if (!sessionStorage.user) {
             quint.listAllItems().success(function (response) {
@@ -47,12 +50,12 @@ quintBuild.controller('homeController', ['$scope','$rootScope', 'quintAPIservice
                 defaultValue();
                 storageToSession();
             });
-        }
-        else{
+        } else {
             retreiveSessionData();
         }
     };
 
+    // Retrieve Data from Session Storage
     var retreiveSessionData = function () {
         $scope.listItems = JSON.parse(sessionStorage.user);
         $scope.numberOfDiamondFound = JSON.parse(sessionStorage.noOfDiamond);
@@ -60,12 +63,14 @@ quintBuild.controller('homeController', ['$scope','$rootScope', 'quintAPIservice
         $scope.userName = JSON.parse(sessionStorage.userName);
     };
 
+    // Store Data from Session Storage
     var storageToSession = function () {
         sessionStorage.user = JSON.stringify($scope.listItems);
         sessionStorage.noOfDiamond = JSON.stringify($scope.numberOfDiamondFound);
         sessionStorage.totalPoints = JSON.stringify($scope.totalPoints);
     };
 
+    // Finding whether the element has diamond or not
     var checkForDiamond = function () {
         $scope.numberOfDiamondFound++;
         if ($scope.numberOfDiamondFound === 8) {
@@ -73,19 +78,66 @@ quintBuild.controller('homeController', ['$scope','$rootScope', 'quintAPIservice
         }
     };
 
+    // Functionality for box click
     $scope.boxIsClicked = function (val) {
         val.isClicked = true;
         $scope.totalPoints = $scope.totalPoints - 1;
-        if(val.isDiamond) {
+        if (val.isDiamond) {
             checkForDiamond();
         }
+        clickedData = val;
         storageToSession();
     };
+
+    //Check for hint
+    $scope.findHint = function () {
+        if (clickedData) {
+            var allDiamonds = findAllDiamonds();
+            var rowIndex = Math.ceil((parseInt(clickedData.index) + 1) / 8);
+            for (i = 0; i < allDiamonds.length; i++) {
+                var currentIndex = Math.ceil((parseInt(allDiamonds[i].index) + 1) / 8);
+                if (currentIndex === rowIndex) {
+                    if ((allDiamonds[i].index - clickedData.index) > 0) {
+                        $scope.message = 'Move to Right';
+                    } else {
+                        $scope.message = 'Move to Left';
+                    }
+                    break;
+                } else {
+                    if (currentIndex > rowIndex) {
+                        $scope.message = 'Move down';
+                    } else {
+                        $scope.message = 'Move Up';
+                    }
+                    break;
+                }
+            }
+        } else {
+            $scope.message = 'Hint will be respect to previous selected column';
+        }
+    };
+
+    //Find all diamonds
+    var findAllDiamonds = function () {
+        var returnData = [];
+        for (i = 0; i < $scope.listItems.length; i++) {
+            for (j = 0; j < $scope.listItems[i].columns.length; j++) {
+                if ($scope.listItems[i].columns[j].isDiamond && !$scope.listItems[i].columns[j].isClicked) {
+                    returnData.push($scope.listItems[i].columns[j]);
+                }
+            }
+        }
+        return returnData;
+    }
+
+    // Default value that needs to be set
     var defaultValue = function () {
         $scope.numberOfDiamondFound = 0;
         $scope.showPoints = false;
         $scope.totalPoints = 64;
     };
+
+    // Resetting the value to play again
     $scope.resetValue = function () {
         defaultValue();
         sessionStorage.clear();
